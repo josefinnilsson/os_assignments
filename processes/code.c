335adee728c691db9d0d7b671308e0526e08e096
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2017-12-08 11:40:50 +0100

    Update code.c

--- processes/code.c
+++ processes/code.c
@@ -3,10 +3,17 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main() {
-	foo: 
-		printf("the code: %p\n", &&foo); 
+char global[] = "This is a global string"; 
 
-		fgetc(stdin); 
+int main() {
+	int pid = getpid(); 
+	foo: 
+		printf("process id: %d\n", pid); 
+		printf("gloabl string: %p\n", &global); 
+		printf("the code: %p\n", &&foo); 
+		printf("\n\n /usr/bin/vmmap%d \n\n", pid); 
+		char command[50]; 
+		sprintf(command, "cat /usr/bin/vmmap%d", pid); 
+		system(command); 
 		return 0; 
 }
